dc8c06283a8db6fb3c6a2c3849d42f79bc20dbfa
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-04 22:26:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
 module "vpc" {
-  source = "../modules/vpc"
+  source = "./modules/vpc"
 }
 
 module "subnet" {
-  source = "../modules/subnet"
+  source = "./modules/subnet"
 }
 
 module "ecr" {
-  source = "../modules/ecr"
+  source = "./modules/ecr"
 }
 
 module "ecs" {
-  source = "../modules/ecs"
+  source = "./modules/ecs"
 }
 
 module "alb" {
-  source = "../modules/alb"
+  source = "./modules/alb"
 }
 
 module "iam" {
-  source = "../modules/iam"
+  source = "./modules/iam"
 }
 
 module "sg" {
-  source = "../modules/sg"
+  source = "./modules/sg"
 }
 
 module "logs" {
-  source = "../modules/logs"
+  source = "./modules/logs"
 }
 
 module "s3" {
-  source = "../modules/s3"
+  source = "./modules/s3"
 }
 
 module "dynamodb" {
-  source = "../modules/dynamodb"
+  source = "./modules/dynamodb"
 }
